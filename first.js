0418cfe031b43b1ee550a67949c0c8dd4b63d325
22machinist <sharmashutosh22@gmail.com>
2024-07-26 10:05:45 +0530

    Added the first practice file

---
+++ first.js
@@ -0,0 +1,53 @@
+console.log('Down the sea');
+console.log("Lie on the ocean");
+console.log(`float on ocean`);
+console.log("Let us code with nothing in mind just ")
+console.log("I wish i can kill my self");
+console.log(100 + 11 * 44);
+console.log("Con" + "centrate" + "d");
+
+console.log(`half of hundred is ${100 / 2}`);
+console.log(typeof (4.5));
+console.log(typeof ("x"));
+console.log(-(10 - 2));
+console.log("Nishtha" == "Ashutosh");
+console.log("Ashutosh" != " ");
+console.log("Ashutosh" < "Nishtha");
+console.log(true && true);
+console.log(NaN == NaN);
+console.log(false || true);
+console.log(true || false);
+
+console.log(false || false);
+
+console.log(1 + 1 == 2 && 10 * 10 > 50);
+console.log(true ? 1 : 2); //This is ternary operation in which if the equation is true than the middle one will be selected
+console.log(false ? 1 : 2); //If the equation is false than the right one will be chosen
+console.log(1 + 1 == 2 && 10 * 10 < 50);
+console.log(1 + 1 == 2 || 10 * 10 > 50);
+console.log(1 + 1 == 2 || 10 * 10 < 50);
+
+//Now let us test it in a different manner 
+5 > 2 ? console.log("You entered a number greater than 5") : ("You entered number less than 5");
+//Let us check different type of codes 
+console.log("It is a harsh world and it needed to be delt with force ");
+//This is the initial code for spectural damage 
+//Let us code for the from the very begining let us code 
+//In this file we will examine our old knowledge testing the things that we learnt before 
+//l. if we argue with the code than the file will collapse .
+// Since the file will collapse it will 
+var sum = 0;
+var x = 12;
+var y = 34;
+sum = x + y;
+console.log(sum);
+
+//Now we will define subtraction
+var diff = 0;
+var a = 36;
+var b = 34;
+diff = a - b;
+console.log(diff);
+
+//Now we will define the multiplication method 
+
